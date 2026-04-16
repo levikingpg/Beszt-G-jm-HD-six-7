a692db99d3182444f1ec4860197cfdcf391136ec
levikingpg <otvos.levente@diak.szbi-pg.hu>
2026-04-16 08:57:29 +0200

    Add files via upload
    AA GÉÉJMM

---
+++ script.js
@@ -0,0 +1,33 @@
+var aKisCsavo = document.getElementById("babu");
+var azAkadaly = document.getElementById("akadaly");
+var pontszamSzamlalo = 0;
+
+function ugras() {
+    
+    if (aKisCsavo.classList == "ugralas") { return; }
+    
+    aKisCsavo.classList.add("ugralas");
+    
+    setTimeout(function() {
+        aKisCsavo.classList.remove("ugralas");
+    }, 300);
+}
+
+var folyamatosEllenorzes = setInterval(function() {
+    
+    let csavoTeteje = parseInt(window.getComputedStyle(aKisCsavo).getPropertyValue("top"));
+    let akadalyBalSzele = parseInt(window.getComputedStyle(azAkadaly).getPropertyValue("left"));
+
+    
+    if (akadalyBalSzele < 20 && akadalyBalSzele > -20 && csavoTeteje >= 130) {
+        azAkadaly.style.animation = "none";
+        alert("Vége a dalnak. Pontszámod: " + Math.floor(pontszamSzamlalo / 100));
+        
+        pontszamSzamlalo = 0;
+        azAkadaly.style.animation = "akadaly-mozgas 1s infinite linear";
+    } else {
+        
+        pontszamSzamlalo++;
+        document.getElementById("pontszamMezo").innerHTML = Math.floor(pontszamSzamlalo / 100);
+    }
+}, 10);
